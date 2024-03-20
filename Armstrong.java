8bfb2bfa57354898e314be55fa441c9249f98467
I-ManishSharma <maneesh27384@gmail.com>
2024-03-20 15:22:27 +0530

    make code more readable by using methods

--- Armstrong.java
+++ Armstrong.java
@@ -1,28 +1,35 @@
 import java.util.Scanner;
 
 public class Armstrong {
+    static int square (int n, int count) {
+        int ans = 1;
+        for (int i = 1; i <= count; i++) {
+            ans *= n;
+        }
+        return ans;
+    }
+    static int countNum (int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num /= 10;
+        }
+        return count;
+    }
     public static void main(String[] args) {
 
         //Program to check whether a number is a Armstrong or not.
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter the number: ");
-        int n = sc.nextInt();
-        int temp = n, r, sum=0;
-
-        while (temp>0) {
-            r = temp%10;
-            temp = temp/10;
-            sum = sum + r*r*r;            
+        int num = sc.nextInt();
+        int i = num, sum = 0;
+        int count = countNum(num);
+        while (i > 0) {
+            int temp = i % 10;
+            sum += square(temp, count);
+            i /= 10;
         }
-
-        if (sum==n) {
-            System.out.println("true");            
-        }
-        else {
-            System.out.println("False");
-        }
+        if (num == sum) System.out.println("Armstrong number"); else System.out.println("Not a Armstrong number");
         sc.close();
     }
 }
--- Armstrong_number.java
+++ Armstrong_number.java
@@ -2,17 +2,35 @@ import java.util.Scanner;
 
 public class Armstrong_number {
 
-    static boolean isArmstrong(int a) {
-        int temp=a, r, sum=0;
-        while (temp!=0) {
-            r = temp%10;
-            sum = sum + (r*r*r);
-            temp /= 10;
+    //To find the square of number
+    static int square (int n, int count) {
+        int ans = 1;
+        for (int i = 1; i <= count; i++) {
+            ans *= n;
         }
-        if (sum == a) {
-            return true;
+        return ans;
+    }
+
+    //To count the number of digits
+    static int countNum (int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num /= 10;
         }
-        return false;
+        return count;
+    }
+
+    //To check the number is Armstrong or not and if armstrong then print it.
+    static void isArmstrong (int num) {
+        int i = num, sum = 0;
+        int count = countNum(num);
+        while (i > 0) {
+            int temp = i % 10;
+            sum += square(temp, count);
+            i /= 10;
+        }
+        if (num == sum) System.out.print(sum + " ");
     }
     //Program to find all Armstrong numbers btw two numbers.
    
@@ -24,9 +42,7 @@ public class Armstrong_number {
         int b = sc.nextInt();
 
         for (int i = a; i <= b; i++) {
-            if (isArmstrong(i)) {
-                System.out.print(i+" ");
-            }
+            isArmstrong(i);
         }
 
         sc.close();
